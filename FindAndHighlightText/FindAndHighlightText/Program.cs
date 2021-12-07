7b2cd79649203b454e55a73a1e8d323e21701897
Gleb Shevchenko <shevchenko@timpro.ru>
2021-12-07 17:15:54 +0300

    Project AutoPDF

---
+++ FindAndHighlightText/FindAndHighlightText/Program.cs
@@ -0,0 +1,297 @@
+﻿using BitMiracle.Docotic.Pdf;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace FindAndHighlightText
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // NOTE: 
+            // When used in trial mode, the library imposes some restrictions.
+            // Please visit http://bitmiracle.com/pdf-library/trial-restrictions.aspx
+            // for more information.
+
+            string pathToFile = "FindAndHighlightText.pdf";
+
+            using (var pdf = new PdfDocument("C:/Users/shevchenko/Desktop/Рамочка/test2.pdf"))
+            {
+                const string TextToFind = "!?!?!?!qweq";
+                //const StringComparison Comparison = StringComparison.InvariantCultureIgnoreCase;
+                const StringComparison Comparison = StringComparison.CurrentCultureIgnoreCase;
+                var highlightColor = new PdfRgbColor(255, 255, 0);
+
+                highlightPhrases(pdf, TextToFind, Comparison, highlightColor, "Shevchenko");
+
+                pdf.Save(pathToFile);
+                ////!?!?-!?!?!qw
+                //const string TextToFind2 = "qwertyqwertyqwertyqwertyqwertyqwertyqwertyqwerty";
+                //const StringComparison Comparison2 = StringComparison.InvariantCultureIgnoreCase;
+                //var highlightColor2 = new PdfRgbColor(255, 255, 255);
+
+                //highlightPhrases(pdf, TextToFind2, Comparison2, highlightColor2, "General Glag 3");
+
+                //pdf.Save(pathToFile);
+
+                //const string TextToFind3 = "!?!?-!?!?!qw";
+                //const StringComparison Comparison3 = StringComparison.InvariantCultureIgnoreCase;
+                //var highlightColor3 = new PdfRgbColor(255, 255, 255);
+
+                //highlightPhrases(pdf, TextToFind3, Comparison3, highlightColor3, "Seleznev");
+
+                //pdf.Save(pathToFile);
+            }
+
+            //Process.Start(pathToFile);
+        }
+
+        private static void highlightPhrases(PdfDocument pdf, string textToFind, StringComparison comparison,
+            PdfColor highlightColor, string rename = "S")
+        {
+            if (string.IsNullOrEmpty(textToFind))
+                throw new ArgumentNullException(nameof(textToFind));
+
+            string[] wordsToFind = textToFind
+                    .Split(' ')
+                    .Where(w => !string.IsNullOrEmpty(w))
+                    .ToArray();
+            foreach (PdfPage page in pdf.Pages)
+            {
+                foreach (PdfRectangle[] phraseBounds in findPhrases(page, wordsToFind, comparison))
+                {
+                    PdfHighlightAnnotation annot = page.AddHighlightAnnotation("", phraseBounds[0], highlightColor);
+                    //if (phraseBounds.Length > 1)
+                    //    annot.SetTextBounds(phraseBounds.Select(b => (PdfQuadrilateral)b));
+
+
+                    // Or you can highlight the text using vector graphics:
+                    page.Canvas.Brush.Color = highlightColor;
+                    page.Canvas.Brush.Opacity = 50;
+                    foreach (var bounds in phraseBounds)
+                        page.Canvas.DrawRectangle(bounds, PdfDrawMode.Fill);
+                    //page.Canvas.Brush.Color = new PdfRgbColor(0, 0, 0);
+                    //page.Canvas.DrawString(rename, phraseBounds[0], PdfTextAlign.Center, PdfVerticalAlign.Center);
+                }
+            }
+        }
+
+        private static IEnumerable<PdfRectangle[]> findPhrases(PdfPage page, string[] wordsToFind,
+            StringComparison comparison)
+        {
+            if (wordsToFind.Length == 0)
+                yield break;
+
+            if (wordsToFind.Length == 1)
+            {
+                string word = wordsToFind[0];
+                foreach (PdfTextData w in page.GetWords())
+                {
+                    string pdfText = w.GetText();
+                    int index = -1;
+                    for (; ; )
+                    {
+                        index = pdfText.IndexOf(word, index + 1, comparison);
+                        if (index < 0)
+                            break;
+
+                        PdfRectangle intersection = getIntersectionBounds(w, pdfText, index, word.Length);
+                        yield return new[] { intersection };
+                    }
+                }
+
+                yield break;
+            }
+
+            // We use the following algorithm:
+            // 1. Group words by transformation matrix. We do that to properly detect neighbours for rotated text.
+            // 2. For each group:
+            //   a. Sort words taking into account the group's transformation matrix.
+            //   b. Search phrase in the collection of sorted words.
+            Dictionary<PdfMatrix, List<PdfTextData>> wordGroups = groupWordsByTransformations(page);
+            foreach (var kvp in wordGroups)
+            {
+                PdfMatrix transformation = kvp.Key;
+                List<PdfTextData> words = kvp.Value;
+
+                sortWords(words, transformation);
+
+                int i = 0;
+                var foundPhrase = new PdfRectangle[wordsToFind.Length];
+                foreach (PdfTextData w in words)
+                {
+                    string pdfText = w.GetText();
+                    if (matchWord(pdfText, wordsToFind, i, comparison))
+                    {
+                        int wordLength = wordsToFind[i].Length;
+                        int startIndex = (i == 0) ? (pdfText.Length - wordLength) : 0;
+                        foundPhrase[i] = getIntersectionBounds(w, pdfText, startIndex, wordLength);
+                        ++i;
+                        if (i < wordsToFind.Length)
+                            continue;
+
+                        yield return foundPhrase;
+                    }
+
+                    i = 0;
+                }
+            }
+        }
+
+        private static Dictionary<PdfMatrix, List<PdfTextData>> groupWordsByTransformations(PdfPage page)
+        {
+            var result = new Dictionary<PdfMatrix, List<PdfTextData>>();
+            foreach (PdfTextData word in page.GetWords())
+            {
+                PdfMatrix matrix = normalizeScaleFactors(word.TransformationMatrix);
+                matrix.OffsetX = 0;
+                matrix.OffsetY = 0;
+
+                if (result.TryGetValue(matrix, out List<PdfTextData> matrixChunks))
+                    matrixChunks.Add(word);
+                else
+                    result[matrix] = new List<PdfTextData> { word };
+            }
+
+            return result;
+        }
+
+        private static void sortWords(List<PdfTextData> words, PdfMatrix transformation)
+        {
+            // For some transformations we should invert X coordinates during sorting.
+            PdfPoint xAxis = transformVector(transformation, 0.1, 0);
+            PdfPoint yAxis = transformVector(transformation, 0.1, 0.1);
+
+            int xDirection = 1;
+
+            // Happens when space is rotated on 90 or 270 degrees
+            if (Math.Abs(xAxis.X) < 0.0001)
+            {
+                // Happens when transformed coordinates fall to 2nd or 4th quarter.
+                //          y
+                //          ^
+                //   2nd    |   1st
+                //          |
+                // ------------------> x
+                //          |
+                //   3rd    |   4th
+                //          |
+                //
+                if (Math.Sign(xAxis.Y) != Math.Sign(yAxis.X))
+                    xDirection = -1;
+            }
+
+            words.Sort((x, y) =>
+            {
+                double yDiff = measureVerticalDistance(x.Position, y.Position, transformation);
+                if (Math.Abs(yDiff) > 0.0001)
+                    return yDiff.CompareTo(0);
+
+                double xDiff = measureHorizontalDistance(x.Position, y.Position, transformation);
+                return (xDiff * xDirection).CompareTo(0);
+            });
+        }
+
+        private static PdfMatrix normalizeScaleFactors(PdfMatrix m)
+        {
+            double scale = getScaleFactor(m.M11, m.M12, m.M21, m.M22);
+
+            // Round to 1 fractional digit to avoid separation of similar matrices
+            // like { 1, 0, 0, 1.12, 0, 0 } and { 1, 0, 0, 1.14, 0, 0 }
+            m.M11 = Math.Round(m.M11 / scale, 1);
+            m.M12 = Math.Round(m.M12 / scale, 1);
+            m.M21 = Math.Round(m.M21 / scale, 1);
+            m.M22 = Math.Round(m.M22 / scale, 1);
+
+            return m;
+        }
+
+        public static double getScaleFactor(params double[] values)
+        {
+            double result = double.MaxValue;
+            foreach (double val in values)
+            {
+                double abs = Math.Abs(val);
+                if (abs > 0.001 && abs < result)
+                    result = abs;
+            }
+
+            return result;
+        }
+
+        private static double measureHorizontalDistance(PdfPoint first, PdfPoint second, PdfMatrix m)
+        {
+            return m.M11 * (first.X - second.X) + m.M21 * (first.Y - second.Y);
+        }
+
+        private static double measureVerticalDistance(PdfPoint first, PdfPoint second, PdfMatrix m)
+        {
+            return m.M12 * (first.X - second.X) + m.M22 * (first.Y - second.Y);
+        }
+
+        private static PdfPoint transformVector(PdfMatrix m, double x, double y)
+        {
+            // Multiply:
+            //           | m11     m12      0 |
+            // |x y 1| * | m21     m22      0 | = | (m11 * x + m21 * y + offsetX) (m12 * x + m22 * y + offsetY) |
+            //           | offsetX offsetY  1 |
+
+            return new PdfPoint(
+                m.M11 * x + m.M21 * y + m.OffsetX,
+                m.M12 * x + m.M22 * y + m.OffsetY
+            );
+        }
+
+        private static PdfRectangle getIntersectionBounds(PdfTextData data, string text, int startIndex, int length)
+        {
+            if (startIndex == 0 && text.Length == length)
+                return data.Bounds;
+
+            IEnumerable<PdfTextData> characters = data.GetCharacters();
+            if (!containsLtrCharactersOnly(data, text))
+            {
+                // Process right-to-left chunks in the reverse order.
+                //
+                // Note that this approach might not work for chunks
+                // containing both left-to-right and right-to-left characters.
+                characters = characters.Reverse();
+            }
+
+            IEnumerable<PdfRectangle> charBounds = characters
+                .Skip(startIndex)
+                .Take(length)
+                .Select(c => c.Bounds);
+
+            PdfRectangle union = charBounds.First();
+            foreach (var b in charBounds.Skip(1))
+                union = PdfRectangle.Union(b, union);
+
+            return union;
+        }
+
+        private static bool containsLtrCharactersOnly(PdfTextData data, string textLogical)
+        {
+            // A simple trick to detect right-to-left / bidirectional text.
+            // Compare logical and visual representations of text to detect directionality.
+            var noBidiOptions = new PdfTextConversionOptions { UseBidi = false };
+            string textVisual = data.GetText(noBidiOptions);
+            return textLogical == textVisual;
+        }
+
+        private static bool matchWord(string text, string[] wordsToFind, int wordIndex, StringComparison comparison)
+        {
+            Debug.Assert(wordsToFind.Length > 1);
+
+            string word = wordsToFind[wordIndex];
+            if (wordIndex == 0)
+                return text.EndsWith(word, comparison);
+
+            if (wordIndex == wordsToFind.Length - 1)
+                return text.StartsWith(word, comparison);
+
+            return text.Equals(word, comparison);
+        }
+    }
+}
---
+++ PDFReader_v1.0/PDFReader_v1.0/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using BitMiracle.Docotic.Pdf;
+using iTextSharp.text.pdf;
+using Tesseract;
+
+namespace PDFReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var documentText = new StringBuilder();
+            using (var pdf = new BitMiracle.Docotic.Pdf.PdfDocument("C:/Users/shevchenko/Desktop/Рамочка/test2.pdf"))
+            {
+                using (var engine = new TesseractEngine(@"tessdata", "rus", EngineMode.LstmOnly))
+                {
+                    for (int i = 0; i < pdf.PageCount; ++i)
+                    {
+                        if (documentText.Length > 0)
+                            documentText.Append("\r\n\r\n");
+
+                        BitMiracle.Docotic.Pdf.PdfPage page = pdf.Pages[i];
+                        string searchableText = page.GetText();
+                        
+                        PdfPoint temp = new PdfPoint(300, 300);
+                        float width = GetPageWidth("C:/Users/shevchenko/Desktop/Рамочка/test2.pdf");
+                        float height = GetPageHeight("C:/Users/shevchenko/Desktop/Рамочка/test2.pdf");
+                        PdfSize size = new PdfSize(width, height);
+                        PdfTextBox tb1 = page.AddTextBox(temp, size);
+                        tb1.Text = "asdfasdfasdf";
+                        //PdfColor tc = new PdfColor();
+                        tb1.FontColor = new PdfRgbColor(255, 255, 255);
+                        if (!string.IsNullOrEmpty(searchableText.Trim()))
+                        {
+                            documentText.Append(searchableText);
+                            //continue;
+                        }
+
+                        // TODO: This page is not searchable. Perform OCR here
+
+                        PdfDrawOptions options = PdfDrawOptions.Create();
+                        options.BackgroundColor = new PdfRgbColor(255, 255, 255);
+                        options.HorizontalResolution = 300;
+                        options.VerticalResolution = 300;
+
+                        string pageImage = $"page_{i}.png";
+                        page.Save(pageImage, options);
+
+                        using (Pix img = Pix.LoadFromFile(pageImage))
+                        {
+                            using (Page recognizedPage = engine.Process(img))
+                            {
+                                Console.WriteLine($"Mean confidence for page #{i}: {recognizedPage.GetMeanConfidence()}");
+
+                                string recognizedText = recognizedPage.GetText();
+                                documentText.Append(recognizedText);
+                            }
+                        }
+
+                        //File.Delete(pageImage);
+                    }
+                }
+            }
+
+            using (var writer = new System.IO.StreamWriter("C:/Users/shevchenko/Desktop/Рамочка/Res/result.txt"))
+            {
+                writer.Write(documentText.ToString());
+            }
+                
+        }
+
+        public static float GetPageHeight(string PathToPDF)
+        {
+            var reader = new PdfReader(PathToPDF);
+
+            // A post script point is 0.352777778mm
+            const float postScriptPoints = (float)0.352777778;
+
+            // The height is returned in post script points from iTextSharp
+            float height = reader.GetPageSizeWithRotation(1).Height * postScriptPoints;
+
+            reader.Close();
+
+            return height;
+
+        }
+
+        public static float GetPageWidth(string PathToPDF)
+        {
+            var reader = new PdfReader(PathToPDF);
+
+            // A post script point is 0.352777778mm
+            const float postScriptPoints = (float)0.352777778;
+
+            // The height is returned in post script points from iTextSharp
+            float width = reader.GetPageSizeWithRotation(1).Width * postScriptPoints;
+
+            reader.Close();
+
+            return width;
+
+        }
+    }
+}
